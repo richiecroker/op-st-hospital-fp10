5f69ff2709cb3c0bf329a7cb1e77665762e50328
Richard Croker <ra.croker@gmail.com>
2026-03-04 22:56:11 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 
 import streamlit as st
-
 import duckdb
 from google.cloud import storage
 from google.oauth2 import service_account
 import pandas as pd
 import os
 
-st.write("secrets check")
-st.write(st.secrets["gcp_service_account"]["project_id"])
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+storage_client = storage.Client(credentials=credentials)
+bucket = storage_client.bucket("ebmdatalab")
+blob = bucket.blob("RC_tests/hospitalfp10.duckdb")
+st.write("bucket connected")
+st.write(blob.exists())
