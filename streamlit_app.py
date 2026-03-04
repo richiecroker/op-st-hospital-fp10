450fa2fb237fb8a32c4ece9d3399cbd274a607fd
Richard Croker <ra.croker@gmail.com>
2026-03-04 23:16:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,22 +53,22 @@ st.dataframe(df)
 ALL = "All"
 
 # Region
-region_opts = [ALL] + sorted(df["region"].dropna().unique().tolist())
-sel_region = st.selectbox("Region", region_opts, index=0)
-df_region = df if sel_region == ALL else df[df["region"] == sel_region]
+#region_opts = [ALL] + sorted(df["region"].dropna().unique().tolist())
+#sel_region = st.selectbox("Region", region_opts, index=0)
+#df_region = df if sel_region == ALL else df[df["region"] == sel_region]
 
 # ICB (dependent on region)
-icb_opts = [ALL] + sorted(df_region["icb"].dropna().unique().tolist())
-sel_icb = st.selectbox("ICB", icb_opts, index=0)
-df_icb = df_region if sel_icb == ALL else df_region[df_region["icb"] == sel_icb]
+#icb_opts = [ALL] + sorted(df_region["icb"].dropna().unique().tolist())
+#sel_icb = st.selectbox("ICB", icb_opts, index=0)
+#df_icb = df_region if sel_icb == ALL else df_region[df_region["icb"] == sel_icb]
 
 # Hospital (dependent on ICB)
-pr_pairs = df_icb[["ods_code", "ods_name"]].drop_duplicates().sort_values("ods_name")
-pr_opts = [ALL] + [f"{r.ods_name} ({r.ods_code})" for r in pr_pairs.itertuples()]
-pr_map = {opt: opt.split(" (")[-1][:-1] for opt in pr_opts if opt != ALL}
-sel_pr = st.selectbox("Hospital", pr_opts, index=0)
-ods_codes = df_icb["ods_code"].unique().tolist() if sel_pr == ALL else [pr_map[sel_pr]]
+#pr_pairs = df_icb[["ods_code", "ods_name"]].drop_duplicates().sort_values("ods_name")
+#pr_opts = [ALL] + [f"{r.ods_name} ({r.ods_code})" for r in pr_pairs.itertuples()]
+#pr_map = {opt: opt.split(" (")[-1][:-1] for opt in pr_opts if opt != ALL}
+#sel_pr = st.selectbox("Hospital", pr_opts, index=0)
+#ods_codes = df_icb["ods_code"].unique().tolist() if sel_pr == ALL else [pr_map[sel_pr]]
 
 # Register as virtual table with duckdb
-codes_df = pd.DataFrame({"ods_code": ods_codes})
-conn.register("_selected_hospitals", codes_df)
+#codes_df = pd.DataFrame({"ods_code": ods_codes})
+#conn.register("_selected_hospitals", codes_df)
