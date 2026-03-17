4dfa2e9f10b6c3f8037430a040a860e7e03b0fae
Richard Croker <ra.croker@gmail.com>
2026-03-17 11:34:13 +0000

    add cd_Category to top.sql

--- queries/top.sql
+++ queries/top.sql
@@ -1,8 +1,8 @@
-SELECT bnf_name, rx.hospital, sum(actual_cost) AS actual_cost, sum(items) AS items
+SELECT bnf_name, rx.hospital, cd_category, sum(actual_cost) AS actual_cost, sum(items) AS items
 FROM prescribing AS rx
 WHERE EXISTS (
     SELECT 1 FROM unnest($1::VARCHAR[]) AS t(code)
     WHERE LEFT(rx.hospital, LENGTH(code)) = code
 )
 AND CAST(month AS DATE) BETWEEN $2 AND $3
-GROUP BY bnf_name, rx.hospital
+GROUP BY bnf_name, rx.hospital,cd_category,
