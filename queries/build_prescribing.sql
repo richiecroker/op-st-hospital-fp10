91920c5134a24e7267c9cde991db537b80f0d7a3
Richard Croker <ra.croker@gmail.com>
2026-03-31 15:56:19 +0100

    amend build prescribing

--- queries/build_prescribing.sql
+++ queries/build_prescribing.sql
@@ -1,9 +1,12 @@
+WITH bnf_chapter AS
+(SELECT DISTINCT chapter, chapter_code FROM hscic.bnf)
+
 SELECT
   PARSE_DATE('%Y%m', CAST(period AS STRING)) AS month,
-  COALESCE(bnf.presentation, bnf_current.presentation, bnf_former.presentation, rx.bnf_name) AS bnf_name, --- use name from BNF map for consistency, then use rx data name if that doesn't exist
+  COALESCE(bnf.presentation, rx.bnf_name) AS bnf_name, --- use name from BNF map for consistency, then use rx data name if that doesn't exist
   TRIM(COALESCE(map.current_bnf_code, rx.bnf_code)) AS bnf_code, --- map to current BNF code if it exists in the BNF normalisation map
   COALESCE(vmp.controlinfo_cat, "No Controlled Drug Status") AS cd_category, --- gives CD status, with "no status" if it doesn't exist
-  COALESCE(bnf.chapter, bnf_current.chapter, bnf_former.chapter, "Unknown chapter") AS bnf_chapter, --- gives BNF chapter, with "unknown" if there's issue with drug (e.g. discontinued), mapped to the old code if the BNF map hasn't been updated
+  COALESCE(bnf_chapter.chapter, "Unknown chapter") AS bnf_chapter, --- gives BNF chapter, with "unknown" if there's issue with drug (e.g. discontinued), mapped to the old code if the BNF map hasn't been updated
   HOSPITAL_TRUST_CODE AS hospital,
   SUM(TOTAL_QUANTITY) AS quantity,
   SUM(TOTAL_ITEMS) AS items,
@@ -11,30 +14,24 @@ SELECT
 
 FROM ebmdatalab.hospitalcommunityprescribing.hospital_community_rx AS rx
 
-LEFT JOIN dmd.vmp_full AS vmp
-  ON CONCAT(
-       SUBSTR(rx.BNF_CODE, 0, 9),
-       "AA",
-       SUBSTR(rx.BNF_CODE, -2),
-       SUBSTR(rx.BNF_CODE, -2)
-     ) = vmp.bnf_code
-
 LEFT JOIN ebmdatalab.hscic.bnf_map AS map
   ON map.former_bnf_code = rx.bnf_code
 
-LEFT JOIN ebmdatalab.hscic.bnf_map AS old_map
-  ON old_map.current_bnf_code = rx.bnf_code
+LEFT JOIN dmd.vmp_full AS vmp
+ON CONCAT(
+      SUBSTR(rx.BNF_CODE, 0, 9),
+      "AA",
+      SUBSTR(rx.BNF_CODE, -2),
+      SUBSTR(rx.BNF_CODE, -2)
+    ) = vmp.bnf_code
+
+INNER JOIN bnf_chapter
+  ON bnf_chapter.chapter_code = left(rx.bnf_code,2)
 
 LEFT JOIN hscic.bnf AS bnf
-  ON TRIM(rx.bnf_code) = bnf.presentation_code
+  ON rx.bnf_code = bnf.presentation_code
 
-LEFT JOIN hscic.bnf AS bnf_current
-  ON TRIM(map.current_bnf_code) = bnf_current.presentation_code --- join on the current/newer code
-
-LEFT JOIN hscic.bnf AS bnf_former
-  ON TRIM(old_map.former_bnf_code) = bnf_former.presentation_code --- join on the former/older code
-
-WHERE PARSE_DATE('%Y%m', CAST(period AS STRING)) >= '2019-01-01'
+WHERE PARSE_DATE('%Y%m', CAST(period AS STRING)) = '2019-01-01'
 
 GROUP BY
   month,
