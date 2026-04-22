ec0bcd1ea7c04b6abdb814b385e892be1f0bfb01
Richard Croker <ra.croker@gmail.com>
2026-04-22 15:51:25 +0100

    Update month_data.sql

--- queries/month_data.sql
+++ queries/month_data.sql
@@ -4,5 +4,6 @@ WHERE EXISTS (
     SELECT 1 FROM unnest($1::VARCHAR[]) AS t(code)
     WHERE LEFT(rx.hospital, LENGTH(code)) = code
 )
+AND ($2::VARCHAR[] IS NULL OR bnf_name = ANY($2::VARCHAR[]))
 GROUP BY month
 ORDER BY month
